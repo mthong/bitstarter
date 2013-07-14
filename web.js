d985ec71183a36c6f0cd6a0e36fa6772ab3c6bce
Ubuntu <ubuntu@ip-172-31-37-44.us-west-2.compute.internal>
2013-07-14 06:39:45 +0000

    Fixed syntax error

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var filebuf = (fs.readFileSync('index.html');
+var filebuf = fs.readFileSync('index.html');
 var filestring = filebuf.toString('utf-8');
 
 app.get('/', function(request, response) {
